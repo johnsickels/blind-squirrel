10af274f2feafb079b44b6d0a7ca87314b3b6248
johnSickels <46609057+johnsickels@users.noreply.github.com>
2022-02-21 22:44:43 -0500

    new message layout

--- sms.js
+++ sms.js
@@ -5,9 +5,19 @@ export const sendSms = (message) => {
   const authToken = process.env.TWILIO_AUTH_TOKEN;
   const client = twilio(accountSid, authToken);
 
+  const body = `New Listing
+  
+  ${message.title}
+
+  Price: ${message.price}
+
+  ${message.url}
+  `;
+
   console.log("sending text");
+
   return client.messages.create({
-    body: JSON.stringify(message),
+    body,
     from: "+13217303849",
     to: "+12393130490",
   });
