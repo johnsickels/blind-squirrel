df0c17e738d0716ca98be61bf308515371ba57d4
johnSickels <46609057+johnsickels@users.noreply.github.com>
2022-03-13 19:40:02 -0400

    new chat id

--- telegram.js
+++ telegram.js
@@ -6,6 +6,5 @@ const bot = new TelegramBot(TELEGRAM_TOKEN);
 
 export const sendTelegram = (message) => {
   const body = `\n\nNew Listing\n\n${message.title}\n\nPrice: ${message.price}\n\n${message.url}`;
-
-  return bot.sendMessage(5119416758, body);
+  return bot.sendMessage(-558012586, body);
 };
